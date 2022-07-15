5962a4a07a95e682301a855c3cc082810495cba9
Vladimir Kudinov <vlkudinov@gmail.com>
2022-07-15 14:48:57 +0300

    Product page and interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,101 +1,41 @@
 import React, { useState } from 'react';
-import { Typography, Button, Chip, Rating } from 'components';
-import ChevronRight from 'public/icons/chevron-right.svg';
-import ChevronDown from 'public/icons/chevron-down.svg';
 import { withLayout } from '../layout/Layout';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+import { MenuItem } from '../interface/menu.interface';
 
-const Home = () => {
-  const [opened, setOpened] = useState(false);
-  const [rating, setRating] = useState<number>(0);
-
+const HomePage = ({ menu, firstCategory }: HomeProps) => {
   return (
     <>
-      <Typography variant="h1">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </Typography>
-      <Typography variant="h2">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </Typography>
-      <Typography variant="h3">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </Typography>
-      <Typography variant="h4">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </Typography>
-      <Typography variant="h5">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </Typography>
-      <Typography variant="h6">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </Typography>
-      <Typography variant="subtitle1">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </Typography>
-      <Typography variant="subtitle2">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </Typography>
-      <Typography variant="body1">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae
-        distinctio, dolor dolore esse exercitationem minima minus perferendis
-        quidem saepe. Accusantium corporis debitis deleniti laudantium omnis
-        quisquam totam! Beatae, magnam.
-      </Typography>
-      <Typography variant="body2">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-        aperiam aspernatur dicta enim facere id iste iure mollitia
-        necessitatibus nemo omnis pariatur qui quibusdam, quidem saepe, sapiente
-        ullam unde vero!
-      </Typography>
-      <Typography variant="body3">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate,
-        deleniti dignissimos doloremque eum impedit laudantium placeat, sint
-        tempore, temporibus vero voluptas. At autem deserunt enim facere libero
-        quisquam saepe?
-      </Typography>
-
-      <Button
-        appearance="primary"
-        endIcon={opened ? <ChevronDown /> : <ChevronRight />}
-        onClick={() => setOpened((prevState) => !prevState)}
-      >
-        Button
-      </Button>
-
-      <Chip color="primary" variant="outlined">
-        Chip
-      </Chip>
-
-      <Chip color="primary" variant="filled">
-        Chip
-      </Chip>
-
-      <Chip color="green" variant="outlined">
-        Chip
-      </Chip>
-
-      <Chip color="green" variant="filled">
-        Chip
-      </Chip>
-
-      <Chip color="red" variant="outlined">
-        Chip
-      </Chip>
-
-      <Chip color="red" variant="filled">
-        Chip
-      </Chip>
-
-      <Chip variant="filled" size="s">
-        Chip
-      </Chip>
-
-      <Chip variant="outlined">
-        <a href="#">Chip</a>
-      </Chip>
-
-      <Rating rating={rating} setRating={setRating} isEditable />
+      <ul>
+        {menu.map((m) => {
+          return <li key={m._id.secondCategory}>{m._id.secondCategory}</li>;
+        })}
+      </ul>
     </>
   );
 };
 
-export default withLayout(Home);
+export default withLayout(HomePage);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`,
+    {
+      firstCategory,
+    }
+  );
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
